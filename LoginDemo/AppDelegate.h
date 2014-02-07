f0710bf007cc8c24422d462ca7a547b6fadc8bcd
david.liu <liuchong1228@yahoo.cn>
2014-02-07 14:59:26 +0800

    first commit

---
+++ LoginDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginDemo
+//
+//  Created by david on 14-2-7.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoginDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoginDemo
+//
+//  Created by david on 14-2-7.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
